179923e72f3cc84ca899c1b4324485414b54704e
Rie.Okano <dslogsys@gmail.com>
2022-01-07 14:21:22 +0900

    pakkan

--- mysql_imp_testdata.bat
+++ mysql_imp_testdata.bat
@@ -1,2 +1,2 @@
 cd/d C:\xampp\mysql\bin
-mysql -u user -pDaiseilog7151 -P 59371 haisya_info < E:\HTTPRoot\haisya_unq\imp_j_yotei.sql
+mysql -u user -pDaiseilog7151 -P 59371 haisya_info < E:\HTTPRoot\haisya_unq\imp_testdata.sql
